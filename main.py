65091a6a5e2b98c5681d1435b5da203be8c5206a
Bing Steup <bingsteup@gmail.com>
2022-04-13 13:46:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+sns.set_theme()
+
+df = pd.DataFrame.from_dict(
+    [
+        {"a": 1, "b": 3},
+        {"a": 2, "b": 3}
+    ]
+)
+
+print(df.describe())
+
+sns_plot = sns.barplot(data=df)
+sns_plot.get_figure().savefig('images/sample.png')
+
