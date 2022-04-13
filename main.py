8d64aa64b4e952452d64da69228e67c160f8e850
Bing Steup <bingsteup@gmail.com>
2022-04-13 13:53:13 +0000

    Improve

--- main.py
+++ main.py
@@ -2,18 +2,23 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-
 sns.set_theme()
 
-df = pd.DataFrame.from_dict(
-    [
-        {"a": 1, "b": 3},
-        {"a": 2, "b": 3}
-    ]
-)
+def main():
+    df = pd.DataFrame.from_dict(
+        [
+            {"a": 1, "b": 3},
+            {"a": 2, "b": 3}
+        ]
+    )
 
-print(df.describe())
+    print(df.describe())
 
-sns_plot = sns.barplot(data=df)
-sns_plot.get_figure().savefig('images/sample.png')
+    sns_plot = sns.barplot(data=df)
+    sns_plot.get_figure().savefig('images/sample.png')
 
+    print("Done")
+
+
+if __name__ == "__main__":
+    main()
